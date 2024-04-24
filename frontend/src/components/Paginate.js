90634791ef4b4b127c5c3cbc92fcf14f86923504
oztek68 <mzvc64@durham.ac.uk>
2024-04-24 16:12:49 +0100

    Updated backend and frontend

--- frontend/src/components/Paginate.js
+++ frontend/src/components/Paginate.js
@@ -4,7 +4,7 @@ import { LinkContainer } from 'react-router-bootstrap'
 
 function Paginate({ pages, page, keyword = '', isAdmin = false }) {
     if (keyword) {
-        keyword = keyword.split('?keyword=')[1].split('&')[0]
+        keyword = keyword.split('?keyword=')[1].split('&')[0];
     }
 
     return (pages > 1 && (
@@ -12,17 +12,17 @@ function Paginate({ pages, page, keyword = '', isAdmin = false }) {
             {[...Array(pages).keys()].map((x) => (
                 <LinkContainer
                     key={x + 1}
-                    to={!isAdmin ?
-                        `/?keyword=${keyword}&page=${x + 1}`
-                        : `/admin/productlist/?keyword=${keyword}&page=${x + 1}`
-                    }
+                    to={{
+                        pathname: isAdmin ? `/admin/productlist/` : `/`,
+                        search: `?keyword=${keyword}&page=${x + 1}`
+                    }}
                 >
                     <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
                 </LinkContainer>
             ))}
         </Pagination>
-    )
-    )
+    ));
 }
 
 export default Paginate
+
